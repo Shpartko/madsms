660b58388032e69824391843614b928db8b737da
DESKTOP-IAKD815\jam <3300101@gmail.com>
2018-05-14 16:37:05 +0300

    Change method name for check message before sending

--- src/Contracts/MessageInterface.php
+++ src/Contracts/MessageInterface.php
@@ -2,17 +2,19 @@
 
 namespace Shpartko\Madsms\Contracts;
 
+use Shpartko\Madsms\Contracts\ReplyInterface;
+
 /**
  * All types of messages needs use this interface
  *
  * @package Shpartko\Madsms
  */
 interface MessageInterface {
-
     public function __construct($phone, $message);
+	public function isCorrectMessage(): bool;
     public function getPhone(): string;
     public function getMessage(): string;
     public function getBase64(): string;
     public function getMessageType(): string;
-
+    public function reply(): ReplyInterface;
 }
