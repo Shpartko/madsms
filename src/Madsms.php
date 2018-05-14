d41ca37acffa5b6773e6ab111a845926ecf7c5df
DESKTOP-IAKD815\jam <3300101@gmail.com>
2018-05-14 16:45:54 +0300

    Rename canMessageSend to isCorrectMessage, add new method isCorrectMessage for gateways

--- src/Madsms.php
+++ src/Madsms.php
@@ -4,6 +4,7 @@ namespace Shpartko\Madsms;
 
 use Shpartko\Madsms\Contracts\GatewayInterface;
 use Shpartko\Madsms\Contracts\MessageInterface;
+use Shpartko\Madsms\Contracts\ReplyInterface;
 use Shpartko\Madsms\Exceptions\GatewaysException;
 use Log;
 
@@ -36,33 +37,29 @@ final class Madsms {
 
     public function send(MessageInterface $message): MessageInterface
     {
-            if ($message->canMessageSend()) {
-                $this->gateway->send($message);
+        $message->reply()->setGateway($this->gateway);
 
-                if ($message->status=='send') {
-                    Log::debug('MadSMS to '.$message->getPhone(), [
-                        'provider' => $this->gateway->getGatewayName(),
-                        'id' => $message->message_id,
-                        'status' => $message->status,
-                    ]);
-                } else {
-                    Log::warning('MadSMS to '.$message->getPhone().' not sended', [
-                        'provider' => $this->gateway->getGatewayName(),
-                        'id' => $message->message_id,
-                        'status' => $message->status,
-                    ]);
-                }
-            } else {
-                Log::error('Cannot send madSMS to '.$message->getPhone().' - wrong format or incorrect phone number', [
+        if (($message->isCorrectMessage()) and ($this->gateway->isCorrectMessage($message))) {
+            $this->gateway->send($message);
+
+            if ($message->reply()->getResult() == ReplyInterface::MESSAGE_SEND) {
+                Log::debug('MadSMS to '.$message->getPhone(), [
                     'provider' => $this->gateway->getGatewayName(),
-                    'status' => $message->status,
+                    'id' => $message->reply()->getId(),
+                ]);
+            } else {
+                Log::warning('MadSMS to '.$message->getPhone().' not sended', [
+                    'provider' => $this->gateway->getGatewayName(),
+                    'id' => $message->reply()->getId(),
                 ]);
             }
+        } else {
+            Log::error('Cannot send madSMS to '.$message->getPhone().' - wrong format or incorrect phone number', [
+                'provider' => $this->gateway->getGatewayName(),
+            ]);
+        }
 
-            $message->gateway_name = $this->gateway->getGatewayName();
-            $message->gateway_logo = $this->gateway->getGatewayLogo();
-
-            return $message;
+        return $message;
     }
 
     public function getGateway(): GatewayInterface
