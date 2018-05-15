40c4b827ad22159877c8d8fbeeb98a7dd3011483
DESKTOP-IAKD815\jam <3300101@gmail.com>
2018-05-15 18:26:37 +0300

    Add notifications section

--- config/madsms.php
+++ config/madsms.php
@@ -22,4 +22,36 @@ return [
 
     'limit_for_one_iteration' => 10,
 
+    /*
+    |--------------------------------------------------------------------------
+    | Send errors, warnings and other notifycations for dev via email or slack
+    |--------------------------------------------------------------------------
+    */
+
+    'notifications' => [
+
+        'send' => false,
+
+        'notifications' => [
+            Shpartko\Madsms\Notifications\Notifications\CannotConnectToGateway::class => ['mail'],
+            Shpartko\Madsms\Notifications\Notifications\MessageCannotSend::class => ['slack'],
+            Shpartko\Madsms\Notifications\Notifications\MessageWasSend::class => [],
+            Shpartko\Madsms\Notifications\Notifications\MessageIncorrect::class => ['slack'],
+        ],
+
+        'notifiable' => Shpartko\Madsms\Notifications\Notifiable::class,
+
+        'mail' => [
+            'to' => '3300101@gmail.com',
+        ],
+
+        'slack' => [
+            'webhook_url' => 'https://hooks.slack.com/services/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',
+            'channel' => '#mad',
+            'username' => 'robot.mad',
+            'icon' => '',
+        ],
+
+    ],
+
 ];
