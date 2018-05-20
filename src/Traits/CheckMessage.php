bb8cdea8e0a9ee8fe384f7eed0943cc421400db4
DESKTOP-IAKD815\jam <3300101@gmail.com>
2018-05-20 17:34:12 +0300

    Add notification for incorrect message format into verification method of message

--- src/Traits/CheckMessage.php
+++ src/Traits/CheckMessage.php
@@ -2,6 +2,9 @@
 
 namespace Shpartko\Madsms\Traits;
 
+use Shpartko\Madsms\Events\MessageIncorrect;
+use Log;
+
 /**
  * Checking message before sending
  *
@@ -11,15 +14,22 @@ trait CheckMessage
 {
     public function isCorrectMessage(): bool
     {
-    	// Make bad format for message for 1 of 5 times (for tests in dev mode)
-    	if ((env('APP_ENV')!='production') && (rand(0,4)==0)) {
-    		return false;
-    	}
+        $isCorrect = false;
 
-        if (($this->getPhone()!='') and ($this->getMessage()!='')) {
-        	return true;
+        if (($this->getPhone()!='') and ($this->getMessage()!=''))
+        	$isCorrect = true;
+
+        // Make bad format for message for 1 of 5 times (for tests in dev mode)
+        if ((env('APP_ENV')!='production') && (rand(0,4)==0))
+            $isCorrect = false;
+
+        if (!$isCorrect) {
+            $this->sendNotification(new MessageIncorrect($this->reply()->getGateway(), $this));
+            Log::error('Cannot send madSMS to '.$this->getPhone().' - wrong format or incorrect phone number', [
+                'provider' => $this->reply()->getGateway()->getGatewayName(),
+            ]);
         }
 
-        return false;
+        return $isCorrect;
     }
 }
