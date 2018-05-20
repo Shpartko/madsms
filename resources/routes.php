8dbfde20cd228ace74db38b21327c95c22de81cd
DESKTOP-IAKD815\jam <3300101@gmail.com>
2018-05-20 17:29:14 +0300

    Fix one declaration of getTestData function

--- resources/routes.php
+++ resources/routes.php
@@ -20,28 +20,30 @@ Route::get('supermadsms/{new_limit?}', function($new_limit = null){
 })->where(['new_limit' => '[0-9]+'])->name('supermadsms');
 
 
-function getTestData($new_limit) {
-    $max = config('madsms.limit_for_one_iteration',0);
-    if (($new_limit) and ($new_limit<101) and ($new_limit>0)) $max = $new_limit;
+if(!function_exists("getTestData")) {
+	function getTestData($new_limit) {
+	    $max = config('madsms.limit_for_one_iteration',0);
+	    if (($new_limit) and ($new_limit<101) and ($new_limit>0)) $max = $new_limit;
 
-    $pool = [];
+	    $pool = [];
 
-    $faker = \Faker\Factory::create();
+	    $faker = \Faker\Factory::create();
 
-    for ($i=0; $i < $max; $i++) {
-        if (rand(0,1)==1) {
-            $pool[] = new Shpartko\Madsms\Message(
-                $faker->e164PhoneNumber(),
-                $faker->text(rand(100,300))
-            );
-        } else {
-            $pool[] = new Shpartko\Madsms\MMS(
-                $faker->e164PhoneNumber(),
-                $faker->text(rand(100,300)),
-                $faker->imageUrl()
-            );
-        }
-    }
+	    for ($i=0; $i < $max; $i++) {
+	        if (rand(0,1)==1) {
+	            $pool[] = new Shpartko\Madsms\Message(
+	                $faker->e164PhoneNumber(),
+	                $faker->text(rand(100,300))
+	            );
+	        } else {
+	            $pool[] = new Shpartko\Madsms\MMS(
+	                $faker->e164PhoneNumber(),
+	                $faker->text(rand(100,300)),
+	                $faker->imageUrl()
+	            );
+	        }
+	    }
 
-    return $pool;
+	    return $pool;
+	}
 }
